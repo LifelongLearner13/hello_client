2a22ea7a8c6cf88efc4a612df4074309fe1d77ab
Joe Seago <joe.seago@gmail.com>
2016-06-29 14:58:36 -0600

    spoofed headers

--- hello_client.js
+++ hello_client.js
@@ -1,7 +1,12 @@
 var unirest = require('unirest');
 
-unirest.get('http://localhost:8080/headers/host').end(function(response) {
+unirest.get('http://localhost:8080/headers').headers({
+host: "localhost:8080",
+'user-agent': "Mozilla/5.0 (Linux; Intel Ubuntu) AppleWebKit/537.36 (KHTML, like Gecko) Internet Explorer 5",
+accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+}).end(function(response) {
   console.log('Status:', response.statusCode);
   console.log('Headers: ', response.headers);
   console.log('Body:', response.body);
+//  console.log(response);
 });
